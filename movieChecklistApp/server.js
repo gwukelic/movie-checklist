bf72752a370afe4508ffc7a755ca1080321c3bb7
David Wukelic <wukebb24@hotmail.com>
2018-09-16 14:19:15 -0700

    updated server.js to invoke express and define which port to use

--- movieChecklistApp/server.js
+++ movieChecklistApp/server.js
@@ -1,2 +1,10 @@
+// variable to define the port and easily chance the number  
+const port = process.env.PORT || 8000;
 
+const path = require('path');
+const express = require('express');
+const app = express();
 
+app
+  .use(express.static(path.join(__dirname, 'dist/movieChecklistApp' )))
+  .listen(port, () => console.log(`Express listening on port ${ port }`));
